3b8dd00c5459feefb42a4fa0d66a9aa6f55d39d5
Rajkumar2303 <64347709+Rajkumar2303@users.noreply.github.com>
2024-04-10 19:52:44 -0400

    Update app.py

--- app.py
+++ app.py
@@ -5,8 +5,13 @@ from PIL import Image
 from io import BytesIO
 from keras.preprocessing import image as keras_image
 import tensorflow as tf
-app = Flask(__name__)
+import pandas as pd
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from flask_cors import CORS
 
+
+app = Flask(__name__)
+CORS(app)
 # Load the LightGBM model
 model = tf.keras.models.load_model('CMRmn2.h5')
 
@@ -21,46 +26,57 @@ def predict():
     if request.method == 'POST':
         try:
             # Get the JSON data from the request
+            if 'file' not in request.files:
+                return jsonify({'error': 'No file part'})
             
-            data = request.get_json()
-            # Extract the URL from the JSON data
-            image_url = data.get('url')
+            file = request.files['file']
+            if file.filename == '':
+                return jsonify({'error': 'No selected file'})
             
-            if image_url:
-                # Fetch the image from the URL
-                response = requests.get(image_url)
-                if response.status_code == 200:
-                    print('test1 ')
-                    # Read the image from the response content
-                    img = Image.open(BytesIO(response.content))
-                    # Preprocess the image
-                    img = img.resize((224, 224))
-                    img_array = keras_image.img_to_array(img)
-                    img_array = np.expand_dims(img_array, axis=0)
-                    img_array = img_array / 255.0  # Normalize the image
-                    
-                    '''# Extract features using VGG16
-                    features = vggmodel.predict(img_array)
-                    features = features.reshape(features.shape[0], -1)
-                    # Make prediction using the model
-                    prediction = model.predict(features)[0]'''
+            if file and allowed_file(file.filename):
 
-                    prediction = model.predict(img_array)[0]
-                    # Convert NumPy array to Python list
-                    prediction_list = prediction.tolist()
-                    # Determine the predicted class
-                    predicted_class = label[np.argmax(prediction_list)]
-
-                    # Return the prediction result as JSON
-                    return jsonify({'predicted_Disease': predicted_class})
+                img = Image.open(file)
+                # Preprocess the image
+                img = img.resize((224, 224))
+                if img.mode != 'RGB':
+                    img=img.convert('RGB')
                 else:
-                    return jsonify({'error': 'Failed to fetch image from the URL'})
+                    pass
+   
+                img_array = keras_image.img_to_array(img)
+                
+                img_array = np.expand_dims(img_array, axis=0)
+                img_array = img_array / 255.0 
+                tes_df = pd.DataFrame({'image_data': [img_array]})
+                tes_datagen = ImageDataGenerator()
+                img_data_array = np.array([tes_df['image_data'][0]])
+                img_data_array = np.squeeze(img_data_array, axis=1)
+                
+                tes_gen = tes_datagen.flow(img_data_array, shuffle=False, batch_size=1)
+                
+                y_pred = model.predict(tes_gen)
+
+                ind = int(np.argmax(y_pred, axis=1))
+                        
+                # Determine the predicted class
+                predicted_class = label[ind]
+
+                # Return the prediction result as JSON
+                return jsonify({'predicted_Disease': predicted_class})
+                
             else:
                 return jsonify({'error': 'No image URL provided'})
         except Exception as e:
             return jsonify({'error': str(e)})
     else:
         return jsonify({'error': 'Invalid request method'})
+
+
+# Function to check if file extension is allowed
+def allowed_file(filename):
+    ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 
 if __name__ == '__main__':
     app.run(debug=True)
