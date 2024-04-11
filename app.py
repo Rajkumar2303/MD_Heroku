728d21df0ac4013e813c3da0a97a5eb1344db13d
Rajkumar2303 <64347709+Rajkumar2303@users.noreply.github.com>
2024-04-10 20:05:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ from keras.preprocessing import image as keras_image
 import tensorflow as tf
 import pandas as pd
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from flask_cors import CORS
+#from flask_cors import CORS
 
 
 app = Flask(__name__)
-CORS(app)
+#CORS(app)
 # Load the LightGBM model
 model = tf.keras.models.load_model('CMRmn2.h5')
 
